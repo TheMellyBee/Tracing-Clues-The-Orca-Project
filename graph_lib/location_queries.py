275e944feca852869167b6d9c635e037243146af
Melanie Burns <melanie.burns.design@gmail.com>
2022-03-05 13:18:30 -0800

    gets address from reverse coordinates using geocode.

--- graph_lib/location_queries.py
+++ graph_lib/location_queries.py
@@ -10,7 +10,8 @@ def set_location(connection, coordinates):
     geolocator = get_geolocater()
     location = geolocator.geocode(coordinates)
 
-    query = "MERGE (l:Location" + location.raw['address'] + ')'
-    query += "set l.latitude = " + location.latitude
-    query += "set l.longitude = " + location.longitude
+    query = "MERGE (l:Location {address:'%s'\n" % (location.raw['address'])
+    query += "set l.latitude = " + location.latitude + "\n"
+    query += "set l.longitude = " + location.longitude + "\n"
     connection.query(query)
+    return location.raw['address']
